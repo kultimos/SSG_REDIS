d17c3f250a29c2cb7710b254359c4fdb7d284ba0
wushuai <659-4gpe145ln@dingtalk.com>
2024-03-21 16:21:49 +0800

    项目初始化

---
+++ src/main/java/com/kul/SsgRedisApplication.java
@@ -0,0 +1,13 @@
+package com.kul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgRedisApplication.class, args);
+    }
+
+}
